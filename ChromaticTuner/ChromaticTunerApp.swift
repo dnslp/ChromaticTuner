b4acca27b5d1bd618f21c0657629796a7d852cde
dnslp <davidnymanslp@gmail.com>
2025-06-30 16:05:20 -0400

    Initial Commit

---
+++ ChromaticTuner/ChromaticTunerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChromaticTunerApp.swift
+//  ChromaticTuner
+//
+//  Created by David Nyman on 6/30/25.
+//
+
+import SwiftUI
+
+@main
+struct ChromaticTunerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChromaticTunerTests/ChromaticTunerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChromaticTunerTests.swift
+//  ChromaticTunerTests
+//
+//  Created by David Nyman on 6/30/25.
+//
+
+import Testing
+@testable import ChromaticTuner
+
+struct ChromaticTunerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
